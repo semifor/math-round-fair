03daf8f3f4cf8e53a07f3d59faf064629c357d1d
Marc Mims <marc@questright.com>
2010-12-08 14:04:18 -0800

    use a developer version

--- lib/Math/Round/Fair.pm
+++ lib/Math/Round/Fair.pm
@@ -8,7 +8,7 @@ use base qw/Exporter/;
 use List::Util qw/shuffle sum min/;
 use POSIX qw/floor ceil DBL_EPSILON/;
 
-our $VERSION = '0.01-aj1';
+our $VERSION = '0.01_01';
 
 our @EXPORT_OK = qw/round_fair round_adjacent/;
 
